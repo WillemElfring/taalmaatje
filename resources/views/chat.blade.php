f7967a3fe8ac30eb5d2bf91bff38fd41e2c98959
Robbsnor <robbinschmidt@gmail.com>
2018-01-15 22:28:12 +0100

    chat bubbles
    - added more chat bubbles
    - changed color of bubbles
    - removed max-with on #app so it will look nicer on langer devices

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -31,7 +31,7 @@
 
 
     <div id="text-chat-container">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-12">
 
@@ -39,6 +39,15 @@
                     
                         <div class="bubble bubble-them">Hello i am Trissa!</div>
                         <div class="bubble bubble-you">Hello im John, how are you doing today?</div>
+                        <div class="bubble bubble-them">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae iste, ad voluptate deleniti repudiandae amet officia laudantium est, beatae voluptates natus officiis quidem ipsam nemo quas suscipit quisquam. Dicta.</div>
+                        <div class="bubble bubble-you">Suspendisse fringilla eros ut felis scelerisque, id accumsan felis sollicitudin. Integer tempor purus id est suscipit, et consequat libero condimentum.</div>
+                        <div class="bubble bubble-you">Vivamus ultricies pretium dui. Cras porta sollicitudin ornare. Aenean imperdiet urna risus. Quisque in sapien in velit ullamcorper interdum.</div>
+                        <div class="bubble bubble-them">Pellentesque nec turpis ac massa tempor mattis ut ac diam. Proin egestas lacinia lacus, nec vulputate tellus bibendum eu. Suspendisse nec egestas est.</div>
+                        <div class="bubble bubble-you">Curabitur in lectus vitae metus commodo egestas non a erat.</div>
+                        <div class="bubble bubble-you">Cras arcu ligula, dignissim nec interdum ac, consectetur at nisi.</div>
+                        <div class="bubble bubble-them">Vivamus augue velit, tempus sed luctus tempor, finibus eleifend urna. Ut convallis efficitur auctor.</div>
+                        <div class="bubble bubble-them">Vestibulum aliquet suscipit leo.</div>
+                        <div class="bubble bubble-you">Si.</div>
 
                     </div>
                     
