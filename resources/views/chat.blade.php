127e3a6d0b993ca2c8967cce0acaeb70c8e5c26c
Robbsnor <robbinschmidt@gmail.com>
2018-01-15 20:29:54 +0100

    chat progress
    - started on video chat holder + controlls
    - started on text chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -2,16 +2,49 @@
 
 @section('content')
 
-	<div id="video-chat">
+	<div id="video-chat-container">
+
+        <div id="video-chat-them"></div>
+        <div id="video-chat-you"></div>
 		
 		<div class="controlls">
-			<ul class="no-dots">
-				<li><a href="" class="ion-chatbubbles"></a></li>
-				<li><a href="" class="ion-chatbubbles"></a></li>
-				<li><a href="" class="ion-chatbubbles"></a></li>
-				<li><a href="" class="ion-chatbubbles"></a></li>
-			</ul>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-12">
+                        
+            			<ul class="no-dots">
+            				<li><a href="" id="icon-phone" class="ion-ios-telephone"></a></li>
+            				<li><a href="" id="icon-camera" class="ion-ios-videocam"></a></li>
+            				<li><a href="" id="icon-mic" class="ion-android-microphone"></a></li>
+            				<li><a href="" id="icon-chat" class="ion-chatbubbles"></a></li>
+            			</ul>
+
+                    </div>
+                </div>
+            </div>
+
 		</div>
+
 	</div>
+
+
+
+    <div id="text-chat-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+
+                    <div id="text-chat">
+                    
+                        <div class="bubble bubble-them">Hello i am Trissa!</div>
+                        <div class="bubble bubble-you">Hello im John, how are you doing today?</div>
+
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
